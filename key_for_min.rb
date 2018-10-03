e972dd5c0bfda7866781500747e68281e61be34d
Min Hee  Park <minheekangg@gmail.com>
2018-10-03 16:00:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  curr_num = name_hash.first
-  curr_key = ""
-name_hash.each do |key, num|
-  if num < curr_num
-      curr_num = num
-      curr_key = key
-    end
-  end
-    return key
+
 end
