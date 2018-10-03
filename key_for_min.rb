ad316002714edd6096512c67107367fccbddcdaa
Min Hee  Park <minheekangg@gmail.com>
2018-10-03 16:06:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 curr_key = name_hash.first
 curr_value = name_hash[curr_key]
 puts curr_key
-puts curr_value
+#puts curr_value
   name_hash.each do |key, value|
     if value < curr_value
       curr_value = value
