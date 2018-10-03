d7f511940ca8dbbd715ce6564a53a0dfbf01d818
Min Hee  Park <minheekangg@gmail.com>
2018-10-03 16:08:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-curr_key = name_hash.first
+curr_key = name_hash[0][0]
 curr_value = name_hash[curr_key]
 puts curr_key
 #puts curr_value
